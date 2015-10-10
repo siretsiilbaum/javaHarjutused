67ef8cb7253d0e7683d505219a1bf539e89da192
Siret Siilbaum <ssiilbau@klass2-15-l.intra.itcollege.ee>
2015-10-10 17:22:14 +0300

    esimene panemine

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,42 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor = new Foor();
+int count = 0;
+        while (count < 10) {
+            foor.syytaPunane();
+            foor.paus(3.0);
+            foor.kustutaPunane();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.paus(1);
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(3.0);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.kustutaRoheline();
+            foor.paus(0.5);
+            foor.syytaKollane();
+            foor.paus(1);
+            foor.kustutaKollane();
+            foor.paus(0.5);
+            count = count + 1;
+
+        }
+    }
 }
